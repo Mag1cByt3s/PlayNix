b19f03ab8bf40ae13a130644a9d4758b3be1f88a
Mag1cByt3s <ppeinecke@protonmail.com>
2025-05-16 06:55:11 +0200

    add missing hardware.nix

---
+++ nixos/hosts/battlestation/hardware.nix
@@ -0,0 +1,22 @@
+# additional hardware config
+{ 
+  config,
+  lib,
+  pkgs,
+  inputs,
+  ... 
+}: {
+  hardware = {
+    # enable firmware with a license allowing redistribution
+    enableRedistributableFirmware = lib.mkForce true;
+
+    # enable all firmware regardless of license
+    enableAllFirmware = lib.mkForce true;
+
+    # enable CPU microcode updates
+    cpu.amd.updateMicrocode = lib.mkForce true;
+
+    # enable non-root access to qmk firmware
+    keyboard.qmk.enable = lib.mkForce true;
+  };
+}
