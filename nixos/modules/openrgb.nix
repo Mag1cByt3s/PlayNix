b5e81691f6c59af41af47b5fb08bf7aeb6cd1415
Mag1cByt3s <ppeinecke@protonmail.com>
2025-05-22 00:39:11 +0200

    delete non-existent autoStart option from openrgb

--- nixos/modules/openrgb.nix
+++ nixos/modules/openrgb.nix
@@ -10,13 +10,12 @@
 
   services.udev.extraRules = (builtins.readFile "${pkgs.openrgb}/lib/udev/rules.d/60-openrgb.rules");
 
-  services.hardware.openrgb = { 
-    enable = true; 
-    package = pkgs.openrgb-with-all-plugins; 
-    motherboard = "amd"; 
-    server = { 
-      port = 6742; 
-      autoStart = true; 
+  services.hardware.openrgb = {
+    enable = true;
+    package = pkgs.openrgb-with-all-plugins;
+    motherboard = "amd";
+    server = {
+      port = 6742;
     }; 
   };
 }
